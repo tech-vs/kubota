71f06233dae109ad15ae96241633319e541929e4
test <test@hotmail.com>
2022-12-05 16:25:14 +0000

    update Dockerfile

--- backend/start.sh
+++ backend/start.sh
@@ -6,5 +6,5 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 # python manage.py initadmin
 
-# gunicorn -b :8000 -w 4 bosch.wsgi
-python manage.py runserver
+gunicorn -b :4200 -w 4 kubota_backend.wsgi
+# python manage.py runserver
