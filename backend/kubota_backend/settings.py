734c99753f9cf3b34438364070d8187a8e52deb8
poonyavee <poonyavee@conicle.com>
2022-12-05 21:59:03 +0700

    mssql

--- backend/kubota_backend/settings.py
+++ backend/kubota_backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'drf_yasg',
 
     'account',
-    # 'syncdata',
+    'syncdata',
     # 'pallet',
 ]
 
--- backend/test.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-
-cnxn = "Driver={FreeTDS};TDS_VERSION=7.3;Server=172.20.176.4,1433;Database=ADJ;UID=kuet\\administrator;PWD=keT2402D0main"
-
-with pyodbc.connect(cnxn) as db:
-    cursor = db.cursor()
-    cursor.execute("SELECT Delivery_Date, Prod_Seq, Pallet#, Skewer# FROM PSE_Ts_DataUpload")
-    for row in cursor:
-        print(row)
