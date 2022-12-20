96232b15f240adf43262789ac6d915ca31587b7d
poonyavee <poonyavee@conicle.com>
2022-12-21 01:33:20 +0700

    add filter loading question_type and dkk

--- backend/pallet/filters.py
+++ backend/pallet/filters.py
@@ -1,12 +1,13 @@
 from django_filters.rest_framework import FilterSet
 from django_filters import filters
 
-from pallet.models import PalletStatus, DocStatus
+from pallet.models import PalletStatus, DocStatus, QuestionType
 
 
 class PalletLoadingFilter(FilterSet):
     internal_pallet_no = filters.CharFilter(field_name='internal_pallet_no')
     status = filters.ChoiceFilter(choices=PalletStatus.choices, field_name='status')
+    question_type = filters.ChoiceFilter(choices=QuestionType.choices, field_name='question_type')
 
 
 class PalletPartListFilter(FilterSet):
