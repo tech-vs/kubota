a52201fcbb7209a99f5df6a5c235b86191b4aa61
Pao Lersiripong <>
2023-01-12 01:12:11 +0700

    [edit] remove tty from docker exec

--- backend/sync_data.sh
+++ backend/sync_data.sh
@@ -2,6 +2,6 @@
 
 set -ex
  
-docker exec -it django python manage.py sync_mssql
-docker exec -it django python manage.py sync_oracle
-docker exec -it django python manage.py sync_ms_packing_style
+docker exec -i django python manage.py sync_mssql
+docker exec -i django python manage.py sync_oracle
+docker exec -i django python manage.py sync_ms_packing_style
